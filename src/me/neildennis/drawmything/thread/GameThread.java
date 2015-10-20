e55b31fb9d984aba4e5cc2dbe17dd36d726b0d36
Neil Dennis <iforgot290@gmail.com>
2015-10-20 12:27:45 -0500

    Made line drawing better

--- src/me/neildennis/drawmything/thread/GameThread.java
+++ src/me/neildennis/drawmything/thread/GameThread.java
@@ -3,8 +3,7 @@ package me.neildennis.drawmything.thread;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import me.neildennis.drawmything.Main;
 import me.neildennis.drawmything.screen.DrawArea;
@@ -14,11 +13,13 @@ public class GameThread extends Thread{
 
 	private Main main;
 
-	@SuppressWarnings("unused")
 	private int oldx, oldy;
+	private ConcurrentLinkedQueue<Line2D> lines;
+	private boolean running = true;
 
 	public GameThread(){
 		main = Main.getMain();
+		lines = new ConcurrentLinkedQueue<Line2D>();
 		start();
 	}
 
@@ -34,42 +35,132 @@ public class GameThread extends Thread{
 
 		main.log("Starting game thread");
 
-		ScreenManager.getDrawArea().addMouseListener(new MouseAdapter() {
+		final DrawArea drawarea = ScreenManager.getDrawArea();
+
+		while (drawarea.getWidth() == 0){
+			try {
+				Thread.sleep(1L);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		}
+		queueLine(new Line2D.Double(100, 100, 110, 200));
+
+		drawarea.addMouseListener(new MouseAdapter() {
 
 			@Override
 			public void mousePressed(MouseEvent e) {
 				oldx = e.getX();
 				oldy = e.getY();
 
-				DrawArea drawarea = ScreenManager.getDrawArea();
-				drawarea.pixels[e.getX() + e.getY() * drawarea.getWidth()] = 0xff00ff;
+				main.log(oldx+","+oldy);
+
+				queueLine(new Line2D.Double(oldx, oldy, oldx, oldy));
 			}
 
 		});
 
-		ScreenManager.getDrawArea().addMouseMotionListener(new MouseAdapter() {
+		drawarea.addMouseMotionListener(new MouseAdapter() {
 
 			@Override
 			public void mouseDragged(MouseEvent e) {
 				int currentx = e.getX();
 				int currenty = e.getY();
-				
-				DrawArea drawarea = ScreenManager.getDrawArea();
 
-				Line2D line = new Line2D.Float(oldx, oldy, currentx, currenty);
-				
+				if (currentx > drawarea.getWidth() || currenty > drawarea.getWidth() || currentx < 0 || currenty < 0) return;
+
+				queueLine(new Line2D.Double(oldx, oldy, currentx, currenty));
+
 				oldx = currentx;
 				oldy = currenty;
 			}
 
-			public void stroke(int stroke, int[] pixels, int x, int y, int width){
-				int pos = x + y * width;
-				if (pos < pixels.length){
-					pixels[pos] = 0xff00ff;
+		});
+
+		while (running){
+			Line2D line;
+			if ((line = lines.poll())!=null){
+				int oldx = (int) line.getX1();
+				int oldy = (int) line.getY1();
+				int currentx = (int) line.getX2();
+				int currenty = (int) line.getY2();
+
+				main.log("drawing line: "+oldx+","+oldy+" "+currentx+","+currenty);
+
+				if (oldx - currentx == 0) {
+					if (oldy <= currenty)
+						for (int y = oldy; y <= currenty; y++){
+							stroke(2, drawarea.pixels, oldx, y, drawarea.getWidth());
+						}
+					else
+						for (int y = currenty; y <= oldy; y++)
+							stroke(2, drawarea.pixels, oldx, y, drawarea.getWidth());
+					continue;
+				}
+
+				float slope = (oldy - currenty) / (oldx - currentx);
+				int intercept = Math.round(-1*(slope*oldx-oldy));
+
+				if (oldx <= currentx){
+					for (int x = oldx; x <= currentx; x++){
+						int y = Math.round(slope * x + intercept);
+						stroke(2, drawarea.pixels, x, y, drawarea.getWidth());
+					}
+					
+					if (slope == 0) continue;
+					
+					if (oldy < currenty)
+						for (int y = oldy; y < currenty; y++){
+							int x = Math.round((y-intercept)/slope);
+							stroke(2, drawarea.pixels, x, y, drawarea.getWidth());
+						}
+					else if (oldy > currenty)
+						for (int y = currenty; y <= oldy; y++){
+							int x = Math.round((y-intercept)/slope);
+							stroke(2, drawarea.pixels, x, y, drawarea.getWidth());
+						}
+				} else {
+					for (int x = currentx; x <= oldx; x++){
+						int y = Math.round(slope * x + intercept);
+						stroke(2, drawarea.pixels, x, y, drawarea.getWidth());
+					}
+					
+					if (slope == 0) continue;
+					
+					if (oldy < currenty)
+						for (int y = oldy; y < currenty; y++){
+							int x = Math.round((y-intercept)/slope);
+							stroke(2, drawarea.pixels, x, y, drawarea.getWidth());
+						}
+					else if (oldy > currenty)
+						for (int y = currenty; y <= oldy; y++){
+							int x = Math.round((y-intercept)/slope);
+							stroke(2, drawarea.pixels, x, y, drawarea.getWidth());
+						}
 				}
 			}
+		}
+	}
 
-		});
+	public synchronized void queueLine(Line2D line){
+		lines.offer(line);
+	}
+
+	public void stroke(int stroke, int[] pixels, int x, int y, int width){
+		int pos = (x + 0) + (y + 0) * width;
+		if (pos < pixels.length) pixels[pos] = 0xff00ff;
+
+		pos = (x + 0) + (y + 1) * width;
+		if (pos < pixels.length) pixels[pos] = 0xff00ff;
+
+		pos = (x + 0) + (y - 1) * width;
+		if (pos < pixels.length) pixels[pos] = 0xff00ff;
+
+		pos = (x + 1) + (y + 0) * width;
+		if (pos < pixels.length) pixels[pos] = 0xff00ff;
+
+		pos = (x - 1) + (y + 0) * width;
+		if (pos < pixels.length) pixels[pos] = 0xff00ff;
 	}
 
 }
